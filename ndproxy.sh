6cdc3e117c9db64e8fd73ec2080b3568bf139a4d
Ed Schouten <ed@kumina.nl>
2011-06-09 09:32:01 +0200

    Add first version of ndproxy.

---
+++ ndproxy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if test $# -lt 2
+then
+	echo "usage: $0 outerface interface ..." >&2
+	exit 1
+fi
+
+OF="$1"
+DB="/var/run/ndproxy.$OF"
+shift
+
+(
+test -f "$DB" && cat "$DB"
+while test $# -gt 0
+do
+	ip -6 neigh show dev "$1"
+	shift
+done | grep -v ^fe80: | cut -d ' ' -f 1
+) | sort -u | tee "$DB" |
+while read address
+do
+	ip -6 neigh add proxy "$address" dev "$OF"
+done
